ff1a84b034be90407f74d10231f0de831c853891
roxyzc <roxyzc12@gmail.com>
2022-12-07 11:03:38 +0700

    ..

---
+++ halaman/tables.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container-fluid mt-2">
+        <div class="table-responsive">
+            <table class="table table-bordered text-center">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">No HP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>roxyzc</td>
+                        <td>roxyzc12@gmail.com</td>
+                        <td>08123456789</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>rozy</td>
+                        <td>rozy@gmail.com</td>
+                        <td>08987654321</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>zxocy</td>
+                        <td>zxocy@gmail.com</td>
+                        <td>08564738193</td>
+                    </tr>
+                </tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
