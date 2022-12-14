8cbe5ca3e7fb3efc9f98d10da0f7cb3e9c1ca1a8
roxyzc <roxyzc12@gmail.com>
2022-12-14 10:29:07 +0700

    ...

--- halaman/grid.php
+++ halaman/grid.php
@@ -14,17 +14,19 @@
     <div class="container-fluid mt-2">
         <div class="container">
             <div class="row">
-                <div class="card text-white bg-primary mb-3">
-                    <div class="card-header">Header 1 </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Primary card title</h5>
-                        <p class="card-text">Some quick example text to build
-                            on the card title and make up the bulk of the card's content.</p>
+                <div class="col-md">
+                    <div class="card text-white bg-primary mb-3">
+                        <div class="card-header">Header 1 </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Primary card title</h5>
+                            <p class="card-text">Some quick example text to build
+                                on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col">
+                <div class="col-md">
                     <div class="card text-dark bg-warning mb-3">
                         <div class="card-header">Header 2</div>
                         <div class="card-body">
@@ -34,7 +36,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md">
                     <div class="card text-dark bg-warning mb-3">
                         <div class="card-header">Header 2</div>
                         <div class="card-body">
@@ -46,7 +48,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col">
+                <div class="col-md">
                     <div class="card text-white bg-success mb-3">
                         <div class="card-header">Header 3</div>
                         <div class="card-body">
@@ -56,7 +58,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md">
                     <div class="card text-white bg-success mb-3">
                         <div class="card-header">Header 3</div>
                         <div class="card-body">
@@ -66,7 +68,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-lg">
                     <div class="card text-white bg-success mb-3">
                         <div class="card-header">Header 3</div>
                         <div class="card-body">
@@ -78,7 +80,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-4 offset-2">
+                <div class="col-md-5 offset-md-1">
                     <div class="card text-white bg-secondary mb-3">
                         <div class="card-header">Header 4</div>
                         <div class="card-body">
@@ -88,7 +90,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-md-5">
                     <div class="card text-white bg-secondary mb-3">
                         <div class="card-header">Header 4</div>
                         <div class="card-body">
@@ -100,7 +102,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-2">
+                <div class="col-lg-2">
                     <div class="card text-white bg-danger mb-3">
                         <div class="card-header">Header 5</div>
                         <div class="card-body">
@@ -110,7 +112,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2">
                     <div class="card text-white bg-danger mb-3">
                         <div class="card-header">Header 5</div>
                         <div class="card-body">
@@ -120,7 +122,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2">
                     <div class="card text-white bg-danger mb-3">
                         <div class="card-header">Header 5</div>
                         <div class="card-body">
@@ -130,7 +132,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2">
                     <div class="card text-white bg-danger mb-3">
                         <div class="card-header">Header 5</div>
                         <div class="card-body">
@@ -140,7 +142,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2">
                     <div class="card text-white bg-danger mb-3">
                         <div class="card-header">Header 5</div>
                         <div class="card-body">
@@ -150,7 +152,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-2">
+                <div class="col-lg-2">
                     <div class="card text-white bg-danger mb-3">
                         <div class="card-header">Header 5</div>
                         <div class="card-body">
--- halaman/tables.php
+++ halaman/tables.php
@@ -13,7 +13,7 @@
 <body>
     <div class="container-fluid mt-2">
         <div class="table-responsive">
-            <table class="table table-bordered text-center table-hover">
+            <table class="table table-bordered text-center table-hover table-striped">
                 <thead class="table-dark">
                     <tr>
                         <th scope="col">No</th>
@@ -38,6 +38,60 @@
                     <tr>
                         <th scope="row">3</th>
                         <td>zxocy</td>
+                        <td>zxocy@gmail.com</td>
+                        <td>08564738193</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Manusia</td>
+                        <td>roxyzc12@gmail.com</td>
+                        <td>08123456789</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>Biasa</td>
+                        <td>rozy@gmail.com</td>
+                        <td>08987654321</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td>Keren</td>
+                        <td>zxocy@gmail.com</td>
+                        <td>08564738193</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">7</th>
+                        <td>Banget</td>
+                        <td>roxyzc12@gmail.com</td>
+                        <td>08123456789</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">8</th>
+                        <td>Bang</td>
+                        <td>rozy@gmail.com</td>
+                        <td>08987654321</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">9</th>
+                        <td>Ayo</td>
+                        <td>zxocy@gmail.com</td>
+                        <td>08564738193</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">10</th>
+                        <td>Lah</td>
+                        <td>roxyzc12@gmail.com</td>
+                        <td>08123456789</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">11</th>
+                        <td>Aziz</td>
+                        <td>rozy@gmail.com</td>
+                        <td>08987654321</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">12</th>
+                        <td>Thema</td>
                         <td>zxocy@gmail.com</td>
                         <td>08564738193</td>
                     </tr>
